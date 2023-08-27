4ec8eabbb500768a3099f27900f6121c63a4cb3d
theboomstick7 <aleksandur.vartolomeev@gmail.com>
2023-08-27 22:35:47 +0300

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from 'react';
+import { useDropzone } from 'react-dropzone'; // for file upload
+import axios from 'axios'; // to make network request
+import TimePicker from './TimePicker'; // our custom TimePicker
+import { toast, ToastContainer } from 'react-toastify'; // for toast notification
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Helper functions (timeToSeconds, secondsToTime, timeToMinutesAndSeconds)
+
+const TranscriptionPage = () => {
+  const [uploading, setUploading] = useState(false);
+  const [transcription, setTranscription] = useState('')
+  const [audioFile, setAudioFile] = useState(null)
+  const [startTime, setStartTime] = useState('00:00:00');
+  const [endTime, setEndTime] = useState('00:10:00'); // 10 minutes default endtime
+  const [audioDuration, setAudioDuration] = useState(null);
 }
-
-export default App;
